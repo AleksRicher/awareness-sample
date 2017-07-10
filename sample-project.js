4cc3e570bb2fcda8792ed625cd714bce73808324
AleksRicher <AleksRicher@gmail.com>
2017-07-10 12:42:40 +0600

    Add 3-way valve

--- sample-project.js
+++ sample-project.js
@@ -11,11 +11,17 @@ var users = JSON.parse(fs.readFileSync(a.Neuron.projectPath + '/users.json', "ut
 var node = new a.Neuron({
     name: "Узел редуцирования газа", children: {
         valve1: new a.Valve({name: "Кран №1"}),
+        valve2: new a.Valve({name: "Кран №2"}),
+        valve3x: new a.Valve3x({name: "Кран №13", pos: 'pos'}),
+        zad: new a.Neuron({name: "sP", rw: true}),
+        pos: new a.Neuron({name: "pos", rw: true}),
+
+
         csd: new a.CSD({name: "Сигнализация"})
     }
 });
 
 
-a.Neuron.afterInit = function(){
-    var web = new a.Web({secure: false, port: 3000, grafana:'http://192.168.0.125:3001', users: users}, node);
+a.Neuron.afterInit = function() {
+    var web = new a.Web({secure: false, port: 3000, grafana: 'http://192.168.0.125:3001', users: users}, node);
 };
